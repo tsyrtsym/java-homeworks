ae82f571a2f7ddf095d17c3d81e4393a8e0766e5
tsyrtsym <tsyrtsym@gmail.com>
2026-04-16 12:42:51 +0500

    Remove task2 folder from lesson1-methods branch

--- src/task2/Colorable.java
+++
@@ -1,8 +0,0 @@
-package task2;
-
-public interface Colorable {
-    String getFillColor();
-    String getBorderColor();
-    void setFillColor(String color);
-    void setBorderColor(String color);
-}
